b10c557a064ce192972d0126e44ecf69d8452f39
maqg <maquangang@gmail.com>
2018-01-10 16:57:45 +0800

    for cpu number fetching

--- modules/agent/cpuinfo/cpuinfo.py
+++ modules/agent/cpuinfo/cpuinfo.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 
+from core.log import ERROR
+from utils.commonUtil import OCT_SYSTEM
+
+
+def get_cpu_number():
+	ret, data = OCT_SYSTEM("cat /proc/cpuinfo | grep processor | wc -l")
+	if ret != 0:
+		ERROR("get cpu number error")
+		return 1
+	
+	return int(data)
+
+
 def get_cpu_info():
-	return "iiii"
+	info = {}
+	info["ncpu"] = get_cpu_number()
+	
+	return info
+
 
 if __name__ == "__main__":
 	info = get_cpu_info()
