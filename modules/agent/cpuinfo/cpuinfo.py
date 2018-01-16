0783b1eba4a4aa4531df2a7b20a9d4fb18d60c85
maqg <maquangang@163.com>
2018-01-16 16:36:37 +0800

    for cpuinfo fetching upgrade

--- modules/agent/cpuinfo/cpuinfo.py
+++ modules/agent/cpuinfo/cpuinfo.py
@@ -10,7 +10,7 @@ from utils.commonUtil import OCT_SYSTEM, transToStr
 HW_CPUINFO_FILE = "/proc/cpuinfo"
 
 
-def parse_hd_cpuinfo():
+def parse_hd_cpuinfos():
 
 	cpus = []
 
@@ -42,7 +42,7 @@ def parse_hd_cpuinfo():
 			cpu["cores"] = int(segs[1])
 		elif segs[0] == "flags": # end of cpuinfo
 			cpus.append(cpu)
-			cpu["flags"] = segs[1]
+			cpu["flags"] = "" # segs[1]
 			cpu = {}
 
 		line = fd.readline()
@@ -50,6 +50,75 @@ def parse_hd_cpuinfo():
 	fd.close()
 
 	return cpus
+
+
+def parse_hd_cpuinfo():
+
+	cpu = {}
+
+	fd = open(HW_CPUINFO_FILE, "r")
+	if not fd:
+		ERROR("open file %s error\n", HW_CPUINFO_FILE)
+		return cpu
+
+	cpu = {}
+	line = fd.readline()
+	while line:
+
+		segs = line.replace("\t","").replace("\n", "").split(": ")
+		if len(segs) != 2:
+			line = fd.readline()
+			continue
+
+		if segs[0] == "vendor_id":
+			cpu["vendorId"] = segs[1]
+		elif segs[0] == "model name":
+			cpu["modelName"] = segs[1]
+		elif segs[0] == "cpu MHz":
+			cpu["cpuHz"] = int(float(segs[1]) * 1000)
+		elif segs[0] == "cache size":
+			cpu["cacheSize"] = int(segs[1].split(" ")[0]) # kB
+		elif segs[0] == "cpu cores":
+			cpu["coresPerCpu"] = int(segs[1])
+		elif segs[0] == "flags": # end of cpuinfo
+			cpu["flags"] = segs[1]
+			break
+
+		line = fd.readline()
+
+	fd.close()
+
+	return cpu
+
+
+# physical cpu number
+def get_cpu_number():
+	ret, data = OCT_SYSTEM("cat %s | grep 'physical id' | sort | uniq | wc -l" % HW_CPUINFO_FILE)
+	if ret != 0:
+		ERROR("get processor number error")
+		return 1
+
+	return int(data.replace("\n", ""))
+
+
+# cores per cpu
+def get_cores_per_cpu():
+	ret, data = OCT_SYSTEM("cat %s | grep 'cpu cores'| uniq" % HW_CPUINFO_FILE)
+	if ret != 0:
+		ERROR("get processor number error")
+		return 2
+
+	return int(data.replace("\n", ""))
+
+
+# total cores
+def get_cpu_cores():
+	ret, data = OCT_SYSTEM("cat %s | grep -w processor | wc -l" % HW_CPUINFO_FILE)
+	if ret != 0:
+		ERROR("get processor number error")
+		return 1
+
+	return int(data.replace("\n", ""))
 
 
 def get_cpu_info():
@@ -60,13 +129,10 @@ def get_cpu_info():
 		ERROR("parse cpu info from cpuinfo file error")
 		return info
 
-	info["ncpu"] = len(cpuinfo)
-	info["modelName"] = cpuinfo[0]["modelName"]
-	info["cpuHz"] = cpuinfo[0]["cpuHz"]
-	info["coresPerCpu"] = cpuinfo[0]["cores"]
-	info["cpus"] = cpuinfo
+	cpuinfo["ncpu"] = get_cpu_number()
+	cpuinfo["ncores"] = get_cpu_cores()
 	
-	return info
+	return cpuinfo
 
 
 if __name__ == "__main__":
